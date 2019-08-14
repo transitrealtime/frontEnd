c6478c4e9f01d245c3629d527c39df6c611c10cf
Steven Li <ilumii@users.noreply.github.com>
2019-08-14 15:44:02 -0400

    redux store

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import LandingPage from './pages/landing';
 
 class App extends Component {
   render() {
+    const landingPageComponent = () => (<LandingPage/>)
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" render={landingPageComponent}/>
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/pages/landing.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+
+class Landing extends Component {
+
+  render() {
+    return (
+      <div>
+				hi
+      </div>
+    );
+  }
+}
+
+export default Landing;
