d9905c1873ae30b95326798ca2363ed78e96fbcb
ilumii <listeven073@gmail.com>
2019-08-15 01:25:44 -0400

    trainview + page routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import LandingPage from './pages/landing';
+import SingleTrain from './pages/singleTrain';
 
 class App extends Component {
   render() {
-    const landingPageComponent = () => (<LandingPage/>)
+    const landingPageComponent = () => (<LandingPage/>);
+    const singleTrainComponent = () => (<SingleTrain/>);
     return (
       <Router>
         <Switch>
           <Route exact path="/" render={landingPageComponent}/>
+          <Route exact path="/train/:train" render={singleTrainComponent}/>
         </Switch>
       </Router>
     );
--- src/pages/singleTrain.js
+++ src/pages/singleTrain.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import TrainsView from '../components/trainsView';
-import '../styles/landing.css';
-
 
 class Landing extends Component {
+
   render() {
     return (
       <div>
-          <div className="display">
-            <TrainsView/>
-          </div>
+          hi
       </div>
     );
   }
